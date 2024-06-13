0f5b1b98068009e49d05fd01e59c8f6ee16dc154
Antoine Delahaye <46669439+antoine-delahaye@users.noreply.github.com>
2024-06-13 12:21:23 +0200

    feat: implement Cloudflare Workers

--- blog/server.ts
+++ blog/server.ts
@@ -1,54 +1,29 @@
-import {APP_BASE_HREF} from '@angular/common'
-import {CommonEngine} from '@angular/ssr'
-import express from 'express'
-import {fileURLToPath} from 'node:url'
-import {dirname, join, resolve} from 'node:path'
+import {renderApplication} from '@angular/platform-server'
 
 import bootstrap from '@src/main.server'
 
-export function app(): express.Express {
-  const server: express.Express = express()
-  const serverDistFolder: string = dirname(fileURLToPath(import.meta.url))
-  const browserDistFolder: string = resolve(serverDistFolder, '../browser')
-  const indexHtml: string = join(serverDistFolder, 'index.server.html')
-
-  const commonEngine: CommonEngine = new CommonEngine()
-
-  server.set('view engine', 'html')
-  server.set('views', browserDistFolder)
-
-  server.get(
-    '*.*',
-    express.static(browserDistFolder, {
-      maxAge: '1y'
-    })
-  )
-
-  server.get('*', (req, res, next): void => {
-    const {protocol, originalUrl, baseUrl, headers} = req
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{provide: APP_BASE_HREF, useValue: baseUrl}]
-      })
-      .then((html: string) => res.send(html))
-      .catch((err) => next(err))
-  })
-
-  return server
+interface Env {
+  ASSETS: {fetch: typeof fetch}
 }
 
-function run(): void {
-  const port: string | 4000 = process.env['PORT'] || 4000
+async function workerFetchHandler(request: Request, env: Env) {
+  const url: URL = new URL(request.url)
 
-  const server: express.Express = app()
-  server.listen(port, (): void => {
-    console.log(`Node Express server listening on http://localhost:${port}`)
+  const indexUrl: URL = new URL('/', url)
+  const indexResponse = await env.ASSETS.fetch(new Request(indexUrl))
+  const document = await indexResponse.text()
+
+  const content: string = await renderApplication(bootstrap, {
+    document,
+    url: url.pathname
   })
+
+  return new Response(content, indexResponse)
 }
 
-run()
+export default {
+  fetch: (request: Request, env: Env) =>
+    (globalThis as any)['__zone_symbol__Promise'].resolve(
+      workerFetchHandler(request, env)
+    )
+}
