a811b5a1c012881f5e253247fa572aa065b09883
Antoine Delahaye <46669439+antoine-delahaye@users.noreply.github.com>
2024-08-01 21:02:06 +0200

    feat(blog): membership sticky button

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -13,6 +13,10 @@ import {siteName} from '@app/core/providers/title.provider'
 import {PageContentService} from '@service/page-content.service'
 import {MediaService} from '@service/media.service'
 import {MediaComponent} from '@shared/media/media.component'
+import {
+  PageContent,
+  PageContentRepository
+} from '@repository/page-content.repository'
 
 @Component({
   selector: 'app-root',
@@ -32,6 +36,9 @@ export class AppComponent implements OnInit {
   private pageContentService: PageContentService = inject(PageContentService)
   private mediaService: MediaService = inject(MediaService)
   protected route: ActivatedRoute = inject(ActivatedRoute)
+  protected pageContentRepository: PageContentRepository = inject(
+    PageContentRepository
+  )
 
   protected navigationRoutes: Routes = navigationRoutes
   protected legalRoutes: Routes = legalRoutes
@@ -47,9 +54,15 @@ export class AppComponent implements OnInit {
       url: 'https://www.helloasso.com/associations/zona-sul-academy'
     }
   ]
+  protected membershipLink?: PageContent
 
   public ngOnInit(): void {
-    this.pageContentService.getAll().subscribe()
+    this.pageContentService.getAll().subscribe({
+      next: (): void => {
+        this.membershipLink =
+          this.pageContentRepository.get('adhesion-helloasso')
+      }
+    })
     this.mediaService.getAll().subscribe()
   }
 }
