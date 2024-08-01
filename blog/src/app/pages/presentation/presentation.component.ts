13eece16d6ab5d883f97f4d83c90da9b4796b058
Antoine Delahaye <46669439+antoine-delahaye@users.noreply.github.com>
2024-08-01 21:40:37 +0200

    feat(blog): skeleton component

--- blog/src/app/pages/presentation/presentation.component.ts
+++ blog/src/app/pages/presentation/presentation.component.ts
@@ -5,11 +5,12 @@ import {PostService} from '@service/post.service'
 import {FeaturedPost, PostRepository} from '@repository/post.repository'
 import {PageContentRepository} from '@src/app/data/repositories/page-content.repository'
 import {MediaComponent} from '@shared/media/media.component'
+import {SkeletonComponent} from '@shared/skeleton/skeleton.component'
 
 @Component({
   selector: 'app-presentation',
   standalone: true,
-  imports: [RouterLink, MediaComponent],
+  imports: [RouterLink, MediaComponent, SkeletonComponent],
   templateUrl: 'presentation.component.html'
 })
 export class PresentationComponent implements OnInit {
